39335f476addc60402b7daaecd47af7884ed3a5e
dylanhouse00 <darwin@tewtow.com>
2019-10-07 09:53:15 -0500

    Add files via upload

---
+++ TwoProgramsForLoops/enc_temp_folder/19b562dbee1b6914d3672e94fce49a8e/Divisors.cpp
@@ -0,0 +1,48 @@
+/*
+Programmer: Dylan House
+Date: 10/2/19
+Description:
+*/
+
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+	int array[100];
+	int number;
+	bool prime = false;
+	/*
+			Splash Screen
+	*/
+
+	system("COLOR 0E");
+	printf("\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t  Prime Numbers\n\n\n\n");
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	printf("\n\n\n\n\t\t\t    Enter a number and this program will tell you its prime status.\n\n\n\n\n\n");
+	_getch();
+	system("cls");
+	/*
+			Calculations
+	*/
+	int remainder;
+	for (number = 0; number <= 100; number++) { //increments the number by 1
+		for (int i = 2; i <= number / 2; i++) {
+			if (number % i == 0) {
+				prime = false;
+			}
+			else {
+				prime = true;
+			}
+		}
+	}
+
+	if (prime == true) {
+		cout << number << " ";
+	}
+	else {
+		cout << "no";
+	}
+}
+//end program
